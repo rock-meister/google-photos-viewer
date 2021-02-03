3dbf71932f8a96c6b0a948ff0932b3dcf8c28759
Ramana Kumar <ramana.r.kumar@gmail.com>
2021-02-03 17:17:42 +0000

    Updates

---
+++ precache-manifest.92d635ba43597b748358f4b8bfb7e05a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce43f9a97279752b095a56fea36b179",
+    "url": "/google-photos-viewer/index.html"
+  },
+  {
+    "revision": "831abdc2d54eb8448453",
+    "url": "/google-photos-viewer/static/css/main.0e50f2f4.chunk.css"
+  },
+  {
+    "revision": "98dc363aec0b45ff4996",
+    "url": "/google-photos-viewer/static/js/2.79b7c8a2.chunk.js"
+  },
+  {
+    "revision": "831abdc2d54eb8448453",
+    "url": "/google-photos-viewer/static/js/main.9e771580.chunk.js"
+  },
+  {
+    "revision": "67a3c3f1844ba0c70bc4",
+    "url": "/google-photos-viewer/static/js/runtime~main.e35f5dcb.js"
+  }
+]);
